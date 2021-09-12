88e3f30b60c1c7ba431250a719b6d1b28da166bd
ap <47916875+atomicparade@users.noreply.github.com>
2021-09-12 10:50:45 -0400

    Layer shift: add option to use existing layers

--- plug-ins/animate_layer_shift.py
+++ plug-ins/animate_layer_shift.py
@@ -5,7 +5,7 @@ import gtk
 
 pdb = gimp.pdb
 
-def zoom_in_and_back_out(
+def animate_layer_shift(
     img,
     original_layer,
     num_frames,
@@ -13,6 +13,7 @@ def zoom_in_and_back_out(
     total_x_shift,
     shift_back,
     use_easing,
+    use_existing_layers,
 ):
     if num_frames % 2 == 1:
         message = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK)
@@ -21,7 +22,14 @@ def zoom_in_and_back_out(
         return
 
     num_frames = int(num_frames)
-    insert_at = img.layers.index(original_layer)
+    starting_layer = img.layers.index(original_layer)
+
+    if use_existing_layers and ((starting_layer + 1) - num_frames) < 0:
+        message = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK)
+        message.set_markup("Use existing layers was selected, but there are not enough layers")
+        message.run()
+        return
+
     (original_x_offset, original_y_offset) = original_layer.offsets
 
     pdb.gimp_image_undo_group_start(img)
@@ -55,13 +63,25 @@ def zoom_in_and_back_out(
             y_offset[i] = pct * total_y_shift
             x_offset[i] = pct * total_x_shift
 
-    # Shift layers
-    for i in range(1, num_frames): # Active layer counts as the first layer
-        layer = pdb.gimp_layer_new_from_drawable(original_layer, img)
-        pdb.gimp_image_insert_layer(img, layer, None, insert_at)
-        pdb.gimp_layer_set_offsets(layer, x_offset[i] + original_x_offset, y_offset[i] + original_y_offset)
+    curr_layer = starting_layer
 
-        pdb.gimp_layer_resize_to_image_size(layer)
+    # Shift layers
+    if use_existing_layers:
+        curr_layer = starting_layer - 1
+
+        for i in range(1, num_frames): # Active layer counts as the first layer
+            layer = img.layers[curr_layer]
+            curr_layer -= 1
+
+            pdb.gimp_layer_set_offsets(layer, x_offset[i] + original_x_offset, y_offset[i] + original_y_offset)
+            pdb.gimp_layer_resize_to_image_size(layer)
+    else:
+        for i in range(1, num_frames): # Active layer counts as the first layer
+            layer = pdb.gimp_layer_new_from_drawable(original_layer, img)
+            pdb.gimp_image_insert_layer(img, layer, None, starting_layer)
+
+            pdb.gimp_layer_set_offsets(layer, x_offset[i] + original_x_offset, y_offset[i] + original_y_offset)
+            pdb.gimp_layer_resize_to_image_size(layer)
 
     pdb.gimp_image_undo_group_end(img)
 
@@ -75,14 +95,15 @@ register(
     "<Image>/Filters/Animation/Animate layer shift",
     "*",
     [
-        (PF_SPINNER,    "num_frames",       "Total number of frames (must be an even number)",  8,      (2, 1000, 2)        ),
-        (PF_SPINNER,    "total_y_shift",    "Total Y shift",                                    0,      (-10000, 10000, 1)  ),
-        (PF_SPINNER,    "total_x_shift",    "Total X shift",                                    0,      (-10000, 10000, 1)  ),
-        (PF_TOGGLE,     "shift_back",       "Shift back to original position",                  False                       ),
-        (PF_TOGGLE,     "use_easing",       "Use easing",                                       False                       ),
+        (PF_SPINNER,    "num_frames",           "Total number of frames (must be an even number)",  8,      (2, 1000, 2)        ),
+        (PF_SPINNER,    "total_y_shift",        "Total Y shift",                                    0,      (-10000, 10000, 1)  ),
+        (PF_SPINNER,    "total_x_shift",        "Total X shift",                                    0,      (-10000, 10000, 1)  ),
+        (PF_TOGGLE,     "shift_back",           "Shift back to original position",                  False                       ),
+        (PF_TOGGLE,     "use_easing",           "Use easing",                                       False                       ),
+        (PF_TOGGLE,     "use_existing_layers",  "Use existing layers",                              False                       ),
     ],
     [],
-    zoom_in_and_back_out,
+    animate_layer_shift,
 )
 
 main()
