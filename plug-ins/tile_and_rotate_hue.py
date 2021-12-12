bbb32eb0cb2548bfecc3f6ca3240fd649c6124ad
ap <47916875+atomicparade@users.noreply.github.com>
2021-12-12 16:17:50 -0500

    Improve efficiency of tile_and_rotate_hue
    Given m = horizontal_copies and n = vertical_copies:
      - Previous implementation required approximately m*n-1 iterations
      - New implementation requires approximately log(m, 2)+log(n, 2)+m+n-2
        iterations

--- plug-ins/tile_and_rotate_hue.py
+++ plug-ins/tile_and_rotate_hue.py
@@ -1,81 +1,136 @@
 #! /usr/bin/env python
+from math import ceil, log
 from gimpfu import *
 import gtk
 
 pdb = gimp.pdb
 
+def copy_and_paste_layer(img, layer, destination_x, destination_y):
+    pdb.gimp_image_select_rectangle(
+        img,
+        CHANNEL_OP_REPLACE,
+        0,          # X
+        0,          # Y
+        img.width,  # Width
+        img.height, # Height
+    )
+
+    pdb.gimp_edit_copy(layer)
+
+    floating_sel = pdb.gimp_edit_paste(layer, True)
+
+    floating_sel = pdb.gimp_item_transform_2d(
+        floating_sel,
+        0,              # Source X
+        0,              # Source Y
+        1,              # Scale X
+        1,              # Scale Y
+        0,              # Rotation angle
+        destination_x,  # Destination X
+        destination_y,  # Detination Y
+    )
+
+    pdb.gimp_floating_sel_anchor(floating_sel)
+
+def shift_hue(img, layer, x, y, width, height, hue_offset):
+    pdb.gimp_image_select_rectangle(
+        img,
+        CHANNEL_OP_REPLACE,
+        x,
+        y,
+        width,
+        height,
+    )
+
+    # gimp_drawable_hue_saturation() accepts a hue_offset between [-180, 180]
+    if hue_offset > 180:
+        hue_offset -= 360
+
+    pdb.gimp_drawable_hue_saturation(
+        layer,
+        HUE_RANGE_ALL,
+        hue_offset,
+        0,    # Lightness adjustment
+        0,    # Saturation adjustment
+        0,    # Overlap other hue channels
+    )
+
 def tile_and_rotate_hue(
     img,
-    original_layer,
+    active_layer,
     horizontal_copies,
     vertical_copies,
 ):
     horizontal_copies = int(horizontal_copies)
     vertical_copies = int(vertical_copies)
 
-    insert_idx = img.layers.index(original_layer)
-    total_copies = horizontal_copies * vertical_copies
-    width = img.width
-    height = img.height
+    original_width = img.width
+    original_height = img.height
 
     pdb.gimp_image_undo_group_start(img)
 
     # Resize entire canvas
     pdb.gimp_image_resize(
         img,
-        width * horizontal_copies,
-        height * vertical_copies,
+        original_width * horizontal_copies,
+        original_height * vertical_copies,
         0,  # X offset
         0,  # Y offset
     )
 
-    copy_number = 1
-    for i in range(vertical_copies):
-        for j in range(horizontal_copies):
-            # No operation needed on the very first one
-            if i == 0 and j == 0:
-                continue
+    pdb.gimp_layer_resize_to_image_size(active_layer)
 
-            # Create copy
-            layer = pdb.gimp_layer_new_from_drawable(original_layer, img)
-            pdb.gimp_image_insert_layer(
-                img,
-                layer,
-                None, # Parent layer (i.e. group)
-                insert_idx,
-            )
+    horizontal_doublings = int(ceil(log(horizontal_copies, 2)))
+    vertical_doublings = int(ceil(log(vertical_copies, 2)))
 
-            # Shift X and Y
-            pdb.gimp_layer_translate(layer, width * j, height * i)
+    # Double horizontally
+    for i in range(horizontal_doublings):
+        copy_and_paste_layer(img, active_layer, original_width * pow(2, i), 0)
 
-            # Shift hue
-            hue_adjustment = ((copy_number * 1.0) / total_copies) * 360
+    # Double vertically
+    for i in range(vertical_doublings):
+        copy_and_paste_layer(img, active_layer, 0, original_height * pow(2, i))
 
-            # gimp_drawable_hue_saturation() accepts a hue_offset between [-180, 180]
-            if hue_adjustment > 180:
-                hue_adjustment -= 360
+    shift_increment = 360.0 / (horizontal_copies * vertical_copies)
 
-            pdb.gimp_drawable_hue_saturation(
-                layer,
-                HUE_RANGE_ALL,
-                hue_adjustment,
-                0,    # Lightness adjustment
-                0,    # Saturation adjustment
-                0,    # Overlap other hue channels
-            )
+    # Shift hue in vertical stripes
+    for i in range(1, horizontal_copies):
+        shift_hue(
+            img,
+            active_layer,
+            original_width * i,
+            0,
+            original_width,
+            img.height,
+            shift_increment * i,
+        )
 
-            # Merge down
+    # Shift hue in horizontal stripes
+    for i in range(1, vertical_copies):
+        shift_hue(
+            img,
+            active_layer,
+            0,
+            original_height * i,
+            img.width,
+            original_height,
+            shift_increment * i * horizontal_copies,
+        )
 
-            copy_number += 1
+    pdb.gimp_layer_resize_to_image_size(active_layer)
 
-    # Merge down
-    merge_layer_idx = img.layers.index(original_layer) - total_copies + 1
-
-    for i in range(total_copies - 1):
-        layer = img.layers[merge_layer_idx]
-        pdb.gimp_image_merge_down(img, layer, EXPAND_AS_NECESSARY)
+    # Clear selection
+    pdb.gimp_image_select_rectangle(
+        img,
+        CHANNEL_OP_REPLACE,
+        0,
+        0,
+        0,
+        0,
+    )
 
     pdb.gimp_image_undo_group_end(img)
+
 
 register(
     "tile_and_rotate_hue",
@@ -87,8 +142,8 @@ register(
     "<Image>/Filters/Artistic/Tile and rotate hue",
     "*",
     [
-        (PF_SPINNER,    "horizontal_copies",    "Horizontal copies",    1,  (1, 25, 1)  ),
-        (PF_SPINNER,    "vertical_copies",      "Vertical copies",      1,  (1, 25, 1)  ),
+        (PF_SPINNER,    "horizontal_copies",    "Horizontal copies",    1,  (1, 100, 1) ),
+        (PF_SPINNER,    "vertical_copies",      "Vertical copies",      1,  (1, 100, 1) ),
     ],
     [],
     tile_and_rotate_hue,
